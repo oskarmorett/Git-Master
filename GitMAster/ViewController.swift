3b2fa930917d9e02d81e197f619f0373bc149b0a
oskar morett <oskarmorett@gmaill.com>
2016-12-05 14:35:29 -0500

    button and feeling array

--- GitMAster/ViewController.swift
+++ GitMAster/ViewController.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+   
+   
+   var feeling = [" fucking tired", "fuck yeah", " this is fucking hard", "fucking crazy"]
+   
+   @IBOutlet weak var label: UILabel!
 
+   @IBOutlet weak var whasUpTap: UIButton!
+   
+   
+   
+   @IBAction func whatsUpButton(_ sender: Any) {
+      
+      var num = feeling.count
+     // var ranNum = arc4random_uniform(UInt16(num))
+      label.text = "fuck "
+      whasUpTap.alpha = 0
+      
+   }
+   
+   
+   
+   
    override func viewDidLoad() {
       super.viewDidLoad()
       // Do any additional setup after loading the view, typically from a nib.
